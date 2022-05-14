75c16beefa0b668f40c72e63d7d8fb8bced6d62e
Oyon87 <oyon87@yahoo.com>
2022-05-14 17:47:03 +0700

    [Fix] Fix Progress bar JS and remove some unused JS Script

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # Yon Andreas Portfolio Website
-Yon Andreas portfolio website. 
+
+Yon Andreas portfolio website.
 https://oyon87.github.io/yonandreas/
 
 Static website for my portfolio
+
+-   magnific-popup
+-   owl.carousel
+-   bootstrap
+-   jquery
+-   icofont
+-   boxicons
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,175 +1,180 @@
-!(function($) {
-  "use strict";
+!( function ( $ ) {
+    "use strict";
 
-  // Nav Menu
-  $(document).on('click', '.nav-menu a, .mobile-nav a', function(e) {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var hash = this.hash;
-      var target = $(hash);
-      if (target.length) {
-        e.preventDefault();
+    // Nav Menu
+    $( document ).on( 'click', '.nav-menu a, .mobile-nav a', function ( e ) {
+        if ( location.pathname.replace( /^\//, '' ) == this.pathname.replace( /^\//, '' ) && location.hostname == this.hostname ) {
+            var hash = this.hash;
+            var target = $( hash );
+            if ( target.length ) {
+                e.preventDefault();
 
-        if ($(this).parents('.nav-menu, .mobile-nav').length) {
-          $('.nav-menu .active, .mobile-nav .active').removeClass('active');
-          $(this).closest('li').addClass('active');
+                if ( $( this ).parents( '.nav-menu, .mobile-nav' ).length ) {
+                    $( '.nav-menu .active, .mobile-nav .active' ).removeClass( 'active' );
+                    $( this ).closest( 'li' ).addClass( 'active' );
+                }
+
+                if ( hash == '#header' ) {
+                    $( '#header' ).removeClass( 'header-top' );
+                    $( "section" ).removeClass( 'section-show' );
+                    if ( $( 'body' ).hasClass( 'mobile-nav-active' ) ) {
+                        $( 'body' ).removeClass( 'mobile-nav-active' );
+                        $( '.mobile-nav-toggle i' ).toggleClass( 'icofont-navigation-menu icofont-close' );
+                        $( '.mobile-nav-overly' ).fadeOut();
+                    }
+                    return;
+                }
+
+                if ( !$( '#header' ).hasClass( 'header-top' ) ) {
+                    $( '#header' ).addClass( 'header-top' );
+                    setTimeout( function () {
+                        $( "section" ).removeClass( 'section-show' );
+                        $( hash ).addClass( 'section-show' );
+
+                    }, 350 );
+                } else {
+                    $( "section" ).removeClass( 'section-show' );
+                    $( hash ).addClass( 'section-show' );
+                }
+
+                $( 'html, body' ).animate( {
+                    scrollTop: 0
+                }, 350 );
+
+                if ( $( 'body' ).hasClass( 'mobile-nav-active' ) ) {
+                    $( 'body' ).removeClass( 'mobile-nav-active' );
+                    $( '.mobile-nav-toggle i' ).toggleClass( 'icofont-navigation-menu icofont-close' );
+                    $( '.mobile-nav-overly' ).fadeOut();
+                }
+
+                return false;
+
+            }
         }
+    } );
 
-        if (hash == '#header') {
-          $('#header').removeClass('header-top');
-          $("section").removeClass('section-show');
-          if ($('body').hasClass('mobile-nav-active')) {
-            $('body').removeClass('mobile-nav-active');
-            $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-            $('.mobile-nav-overly').fadeOut();
-          }
-          return;
+    // Activate/show sections on load with hash links
+    if ( window.location.hash ) {
+        var initial_nav = window.location.hash;
+        if ( $( initial_nav ).length ) {
+            $( '#header' ).addClass( 'header-top' );
+            $( '.nav-menu .active, .mobile-nav .active' ).removeClass( 'active' );
+            $( '.nav-menu, .mobile-nav' ).find( 'a[href="' + initial_nav + '"]' ).parent( 'li' ).addClass( 'active' );
+            setTimeout( function () {
+                $( "section" ).removeClass( 'section-show' );
+                $( initial_nav ).addClass( 'section-show' );
+            }, 350 );
         }
-
-        if (!$('#header').hasClass('header-top')) {
-          $('#header').addClass('header-top');
-          setTimeout(function() {
-            $("section").removeClass('section-show');
-            $(hash).addClass('section-show');
-
-          }, 350);
-        } else {
-          $("section").removeClass('section-show');
-          $(hash).addClass('section-show');
-        }
-
-        $('html, body').animate({
-          scrollTop: 0
-        }, 350);
-
-        if ($('body').hasClass('mobile-nav-active')) {
-          $('body').removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-          $('.mobile-nav-overly').fadeOut();
-        }
-
-        return false;
-
-      }
     }
-  });
 
-  // Activate/show sections on load with hash links
-  if (window.location.hash) {
-    var initial_nav = window.location.hash;
-    if ($(initial_nav).length) {
-      $('#header').addClass('header-top');
-      $('.nav-menu .active, .mobile-nav .active').removeClass('active');
-      $('.nav-menu, .mobile-nav').find('a[href="' + initial_nav + '"]').parent('li').addClass('active');
-      setTimeout(function() {
-        $("section").removeClass('section-show');
-        $(initial_nav).addClass('section-show');
-      }, 350);
+    // Mobile Navigation
+    if ( $( '.nav-menu' ).length ) {
+        var $mobile_nav = $( '.nav-menu' ).clone().prop( {
+            class: 'mobile-nav d-lg-none'
+        } );
+        $( 'body' ).append( $mobile_nav );
+        $( 'body' ).prepend( '<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>' );
+        $( 'body' ).append( '<div class="mobile-nav-overly"></div>' );
+
+        $( document ).on( 'click', '.mobile-nav-toggle', function ( e ) {
+            $( 'body' ).toggleClass( 'mobile-nav-active' );
+            $( '.mobile-nav-toggle i' ).toggleClass( 'icofont-navigation-menu icofont-close' );
+            $( '.mobile-nav-overly' ).toggle();
+        } );
+
+        $( document ).click( function ( e ) {
+            var container = $( ".mobile-nav, .mobile-nav-toggle" );
+            if ( !container.is( e.target ) && container.has( e.target ).length === 0 ) {
+                if ( $( 'body' ).hasClass( 'mobile-nav-active' ) ) {
+                    $( 'body' ).removeClass( 'mobile-nav-active' );
+                    $( '.mobile-nav-toggle i' ).toggleClass( 'icofont-navigation-menu icofont-close' );
+                    $( '.mobile-nav-overly' ).fadeOut();
+                }
+            }
+        } );
+    } else if ( $( ".mobile-nav, .mobile-nav-toggle" ).length ) {
+        $( ".mobile-nav, .mobile-nav-toggle" ).hide();
     }
-  }
 
-  // Mobile Navigation
-  if ($('.nav-menu').length) {
-    var $mobile_nav = $('.nav-menu').clone().prop({
-      class: 'mobile-nav d-lg-none'
-    });
-    $('body').append($mobile_nav);
-    $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
-    $('body').append('<div class="mobile-nav-overly"></div>');
+    // jQuery counterUp
+    // $('[data-toggle="counter-up"]').counterUp({
+    //   delay: 10,
+    //   time: 1000
+    // });
 
-    $(document).on('click', '.mobile-nav-toggle', function(e) {
-      $('body').toggleClass('mobile-nav-active');
-      $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-      $('.mobile-nav-overly').toggle();
-    });
+    // Skills section
+    $( '.skills-content' ).click( function () {
+        $( '.progress .progress-bar' ).each( function () {
+            $( this ).css( "width", '1px' );
+            $( this ).css( "transition", '0s' );
+            setTimeout( () => {
+                $( this ).css( "transition", '2s linear' );
+                $( this ).css( "width", $( this ).attr( "aria-valuenow" ) + '%' );
+            }, 500 )
+        } );
+    } );
 
-    $(document).click(function(e) {
-      var container = $(".mobile-nav, .mobile-nav-toggle");
-      if (!container.is(e.target) && container.has(e.target).length === 0) {
-        if ($('body').hasClass('mobile-nav-active')) {
-          $('body').removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-          $('.mobile-nav-overly').fadeOut();
+    // Image Popup
+    $( '.image-link' ).magnificPopup( {
+        type: 'image',
+        closeOnContentClick: true,
+        mainClass: 'mfp-img-mobile',
+        image: {
+            verticalFit: true
         }
-      }
-    });
-  } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
-    $(".mobile-nav, .mobile-nav-toggle").hide();
-  }
+    } );
 
-  // jQuery counterUp
-  // $('[data-toggle="counter-up"]').counterUp({
-  //   delay: 10,
-  //   time: 1000
-  // });
+    // Testimonials carousel (uses the Owl Carousel library)
+    $( ".owl-carousel" ).owlCarousel( {
+        autoplay: false,
+        margin: 15,
+        dots: true,
+        nav: true,
+        loop: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            990: {
+                items: 3
+            }
+        }
+    } );
 
-  // Skills section
-  $('.skills-content').click(function() {
-    $('.progress .progress-bar').each(function() {
-      $(this).css("width", $(this).attr("aria-valuenow") + '%');
-    });
-  });
+    // Porfolio isotope and filter
+    // $(window).on('load', function() {
+    //   var portfolioIsotope = $('.portfolio-container').isotope({
+    //     itemSelector: '.portfolio-item',
+    //     layoutMode: 'fitRows'
+    //   });
 
-  // Image Popup
-  $('.image-link').magnificPopup({
-		type: 'image',
-		closeOnContentClick: true,
-		mainClass: 'mfp-img-mobile',
-		image: {
-			verticalFit: true
-		}
-	});
+    //   $('#portfolio-flters li').on('click', function() {
+    //     $("#portfolio-flters li").removeClass('filter-active');
+    //     $(this).addClass('filter-active');
 
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".owl-carousel").owlCarousel({
-    autoplay: false,
-    margin: 15,
-    dots: true,
-    nav: true,
-    loop: false,
-    responsive: {
-      0: {
-        items: 1
-      },
-      768: {
-        items: 2
-      },
-      990: {
-        items: 3
-      }
-    }
-  });
+    //     portfolioIsotope.isotope({
+    //       filter: $(this).data('filter')
+    //     });
+    //   });
 
-  // Porfolio isotope and filter
-  // $(window).on('load', function() {
-  //   var portfolioIsotope = $('.portfolio-container').isotope({
-  //     itemSelector: '.portfolio-item',
-  //     layoutMode: 'fitRows'
-  //   });
+    // });
 
-  //   $('#portfolio-flters li').on('click', function() {
-  //     $("#portfolio-flters li").removeClass('filter-active');
-  //     $(this).addClass('filter-active');
+    // Initiate venobox (lightbox feature used in portofilo)
+    // $(document).ready(function() {
+    //   $('.venobox').venobox({
+    //     'share': false
+    //   });
+    // });
 
-  //     portfolioIsotope.isotope({
-  //       filter: $(this).data('filter')
-  //     });
-  //   });
+    // // Portfolio details carousel
+    // $(".portfolio-details-carousel").owlCarousel({
+    //   autoplay: true,
+    //   dots: true,
+    //   loop: true,
+    //   items: 1
+    // });
 
-  // });
-
-  // Initiate venobox (lightbox feature used in portofilo)
-  // $(document).ready(function() {
-  //   $('.venobox').venobox({
-  //     'share': false
-  //   });
-  // });
-
-  // // Portfolio details carousel
-  // $(".portfolio-details-carousel").owlCarousel({
-  //   autoplay: true,
-  //   dots: true,
-  //   loop: true,
-  //   items: 1
-  // });
-
-})(jQuery);
+} )( jQuery );
